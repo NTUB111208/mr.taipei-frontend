6c8809a3a11d93daa44f2bea91355430f729f852
Sciuridae0603 <sciuridae0603@gmail.com>
2022-11-05 08:54:09 +0800

    Fix pathdraw

--- components/Navigation/Panels/4-StationMap.vue
+++ components/Navigation/Panels/4-StationMap.vue
@@ -1,14 +1,14 @@
 <template>
   <div class="wrapper">
-    <Panel style="overflow: auto; height: 650px;">
+    <Panel style="overflow: auto; height: 650px">
       <div class="justify-content-space-between align-items-flex-end">
         <span class="font-size-huge color-focus"> 車站內部路線 </span>
       </div>
-      
+
       <div
         v-for="(station, index) in data.sationMaps"
         :key="index"
-        class="station" 
+        class="station"
       >
         <span class="font-size-mini color-secondary">
           {{ station.type }}
@@ -19,14 +19,10 @@
         <img :src="station.image" />
         <hr />
       </div>
-
-      
     </Panel>
     <div class="cols mb-1">
       <div class="col-3 p-1">
-        <Button color="#f3f5f8" textColor="#00e88e" >
-          &nbsp;
-        </Button>
+        <Button color="#f3f5f8" textColor="#00e88e"> &nbsp; </Button>
       </div>
       <div class="col-4 p-1">
         <Button
@@ -43,7 +39,6 @@
           >詳細路線資料</Button
         >
       </div>
-      
     </div>
   </div>
 </template>
@@ -60,17 +55,21 @@ export default {
       {
         type: '起點',
         station: '東門站',
-        image: 'http://140.131.114.159:3000/pathdraw?startPoint=Exit2&endPoint=Exit2&startStation=Dongmen&transferStation=Daan&destiStation=Dazhi&startTrainHeadto=%E8%B1%A1%E5%B1%B1&destiTrainHeadto=%E5%8D%97%E6%B8%AF%E5%B1%95%E8%A6%BD%E9%A4%A8&imgNum=0',
+        image:
+          'http://140.131.114.159:3000/python/pathdraw?startPoint=Exit2&endPoint=Exit2&startStation=Dongmen&transferStation=Daan&destiStation=Dazhi&startTrainHeadto=%E8%B1%A1%E5%B1%B1&destiTrainHeadto=%E5%8D%97%E6%B8%AF%E5%B1%95%E8%A6%BD%E9%A4%A8&imgNum=0',
       },
       {
         type: '轉乘站',
         station: '大安站',
-        image: 'http://140.131.114.159:3000/pathdraw?startPoint=Exit2&endPoint=Exit2&startStation=Dongmen&transferStation=Daan&destiStation=Dazhi&startTrainHeadto=%E8%B1%A1%E5%B1%B1&destiTrainHeadto=%E5%8D%97%E6%B8%AF%E5%B1%95%E8%A6%BD%E9%A4%A8&imgNum=1',
-      },{
+        image:
+          'http://140.131.114.159:3000/python/pathdraw?startPoint=Exit2&endPoint=Exit2&startStation=Dongmen&transferStation=Daan&destiStation=Dazhi&startTrainHeadto=%E8%B1%A1%E5%B1%B1&destiTrainHeadto=%E5%8D%97%E6%B8%AF%E5%B1%95%E8%A6%BD%E9%A4%A8&imgNum=1',
+      },
+      {
         type: '終點站',
         station: '大直站',
-        image: 'http://140.131.114.159:3000/pathdraw?startPoint=Exit2&endPoint=Exit2&startStation=Dongmen&transferStation=Daan&destiStation=Dazhi&startTrainHeadto=%E8%B1%A1%E5%B1%B1&destiTrainHeadto=%E5%8D%97%E6%B8%AF%E5%B1%95%E8%A6%BD%E9%A4%A8&imgNum=2',
-      }
+        image:
+          'http://140.131.114.159:3000/python/pathdraw?startPoint=Exit2&endPoint=Exit2&startStation=Dongmen&transferStation=Daan&destiStation=Dazhi&startTrainHeadto=%E8%B1%A1%E5%B1%B1&destiTrainHeadto=%E5%8D%97%E6%B8%AF%E5%B1%95%E8%A6%BD%E9%A4%A8&imgNum=2',
+      },
     ])
   },
 }
@@ -87,7 +86,6 @@ export default {
 .wrapper > div {
   margin-top: 1rem;
 }
-
 
 .station {
   margin-top: 0.01rem;
